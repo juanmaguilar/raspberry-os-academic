9c0747442365ce351b67701953e6014f94a0288a
Juan Aguilar <juanmaguilar@gmail.com>
2025-02-11 10:49:10 -0600

    Primer commit

---
+++ launch2.sh
@@ -0,0 +1,9 @@
+qemu-system-aarch64 -machine raspi3b \
+		    -cpu cortex-a72 \
+		    -nographic \
+		    -dtb bcm2710-rpi-3-b-plus.dtb \
+		    -m 1G -smp 4 \
+		    -kernel kernel8.img \
+		    -sd disk.img \
+		    -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
+		    -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
